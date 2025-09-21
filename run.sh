be0269160438ec5c6f5bbd0bd5bb42d70e237913
Basics_First <38207892+RaghavanSridharan@users.noreply.github.com>
2025-09-21 22:34:12 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+COMPOSE_URL="https://raw.githubusercontent.com/RaghavanSridharan/cert_tracker/main/docker-compose.yml"
+
 # Download docker-compose.yml if not already present
 if [ ! -f docker-compose.yml ]; then
   echo "Fetching docker-compose.yml..."
-  curl -s -O https://raw.githubusercontent.com/basics89/cert-tracker/main/docker-compose.yml
+  curl -s -o docker-compose.yml $COMPOSE_URL
 fi
 
 echo "Starting Certificate Tracker..."
 docker compose up
-
